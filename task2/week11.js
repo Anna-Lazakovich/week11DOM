5653065922fadea4ac0baa0620270c5fadf9b87f
Anna-Lazakovich <anna27659@gmail.com>
2023-10-25 01:51:42 +0300

    task2

---
+++ task2/week11.js
@@ -0,0 +1,20 @@
+const taskInput = document.getElementById('input');
+const button = document.getElementById('button');
+const list = document.getElementById('list');
+
+function createTask() {
+	const newTask = document.createElement('li');
+	newTask.textContent = taskInput.value;
+	list.append(newTask);
+	taskInput.value = '';
+}
+
+button.addEventListener('click', createTask);
+
+
+
+list.addEventListener('click', function checkTask(evt) {
+	if (evt.target.tagName === 'li') {
+	  evt.target.classList.toggle('active');
+	}
+  });
